8641559efe3c3d85cfb0fbfc070f3101f36d7c23
Muhammad Arif <talentedarif@gmail.com>
2021-09-15 01:15:14 +0500

    complete

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
-const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=03bdf16b4915fc5ba1bcc411c8054a54&page=1' 
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8582442a21ce11db2105afa2f41e87d5&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500/';
+const SEARCH_API = 'https://api.themoviedb.org/3/discover/movie?api_key=8582442a21ce11db2105afa2f41e87d5&query="';
 
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_URL = 'http://api.themoviedb.org/3/search/movie?a pi_key03bdf16b4915fc5ba1bcc411c8054a54&query="'
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 getMovies(API_URL)
+async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
 
-async function getMovies(url){
-    const res = await fetch(url)
-    const data = await res.json()
-
-    console.log(data.results)
+    showMovies(data.results)
 }
+
+function showMovies (movies) {
+    main.innerHTML = '';
+
+    movies.forEach ((movie) => {
+        const {title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie')
+        movieEl.innerHTML = `
+      
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="green">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>OverView</h3>
+            ${overview}
+        </div>
+  
+        `
+        main.appendChild(movieEl)
+    })
+}
+
+function getClassByRate (vote) {
+    if(vote >= 8) {
+        return 'green'
+    }else if (vote >= 5) {
+        return 'orange'
+    }else {
+        return 'red'
+    }
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value=''
+        }else{
+            window.location.reload()
+    };
+});
