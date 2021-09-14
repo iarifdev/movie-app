44aae48cfc7e51e4218511486b5d887e35629e49
Muhammad Arif <talentedarif@gmail.com>
2021-09-15 00:42:50 +0500

    fetch api data in array form

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=03bdf16b4915fc5ba1bcc411c8054a54&page=1' 
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_URL = 'http://api.themoviedb.org/3/search/movie?a pi_key03bdf16b4915fc5ba1bcc411c8054a54&query="'
+
+getMovies(API_URL)
+
+async function getMovies(url){
+    const res = await fetch(url)
+    const data = await res.json()
+
+    console.log(data.results)
+}
